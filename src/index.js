ac04a313d112c4077f85ec1f8832b83f9a4bb0f4
Joseph Crump <josephcrump79@yahoo.com>
2024-12-03 09:25:14 -0600

    fetched the API and now we can search by city (no results on app  yet)

--- src/index.js
+++ src/index.js
@@ -4,16 +4,32 @@ const addSubmitListener = () => {
         event.preventDefault()
 
         const city = document.getElementById('city-name').value
-
+        getBreweriesByCity(city)
 
         console.log(city)
 
     })
 }
 
+const getBreweriesByCity = (city, perPage = 10) => {
+    const encodedCity = encodeURIComponent(city);
+    fetch(`https://api.openbrewerydb.org/v1/breweries?by_city=${encodedCity}&per_page=${perPage}`)
+        .then((response) => {
+            if(!response.ok) {
+                console.log(response)
+            }
+            return response.json();
+        })
+        .then((breweries) => {
+            console.log(breweries)
+
+        })
+}
+
 const main = () => {
     console.log('main function called')
     addSubmitListener();
+
 };
 
 window.addEventListener('load', main)
